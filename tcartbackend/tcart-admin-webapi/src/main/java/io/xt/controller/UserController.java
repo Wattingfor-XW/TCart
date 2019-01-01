5afe68568599f4b44e9f18c59bba868640dd6b8d
Wattingfor-XW <931217673@qq.com>
2019-01-01 23:47:07 +0800

    add Login

--- tcartbackend/tcart-admin-webapi/src/main/java/io/xt/controller/UserController.java
+++ tcartbackend/tcart-admin-webapi/src/main/java/io/xt/controller/UserController.java
@@ -4,7 +4,6 @@ import io.xt.dto.AddUserDTO;
 import io.xt.exception.BackendClientException;
 import io.xt.pojo.User;
 import io.xt.service.UserService;
-import org.apache.tomcat.jni.Thread;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
@@ -24,14 +23,15 @@ public class UserController {
         userService.addUser(addUserDTO);
     }
     @GetMapping("/login")
-    public String login(@RequestParam("username") String username,@RequestParam("userpassword") String password) throws BackendClientException {
+    public String login(String username,String password) throws BackendClientException {
         User byUsername = userService.getByUsername(username);
-        if(byUsername!=null){
+        if(byUsername==null){
+            throw new BackendClientException("账户不存在");
+
+        }else{
             if(!byUsername.getEncryptedPassword().equals(DigestUtils.md5DigestAsHex(password.getBytes()))){
                 throw new BackendClientException("密码错误");
             }
-        }else{
-        throw new BackendClientException("账户不存在");
         }
         return "token";
     }
