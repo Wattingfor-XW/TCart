cbb922a1b2e24626f3dfe71ee39dbd547f8e9a19
Wattingfor-XW <931217673@qq.com>
2019-01-01 19:43:02 +0800

    2

---
+++ tcartbackend/tcart-admin-webapi/src/main/java/io/xt/TCartAdminWebApiApplication.java
@@ -0,0 +1,12 @@
+package io.xt;
+
+
+@SpringBootApplication
+public class JCartAdminWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JCartAdminWebApiApplication.class, args);
+    }
+
+}
+
---
+++ tcartbackend/tcart-admin-webapi/src/main/java/io/xt/controller/UserController.java
@@ -0,0 +1,3 @@
+package io.xt.controller;
+public class UserController {
+}
