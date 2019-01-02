ab04073b9bc36acb3d4565c938fb15acd67249cf
Wattingfor-XW <931217673@qq.com>
2019-01-02 23:39:17 +0800

    add token

--- tcartbackend/tcart-admin-webapi/src/main/java/io/xt/interceptor/TokenIntercepetor.java
+++ tcartbackend/tcart-admin-webapi/src/main/java/io/xt/interceptor/TokenIntercepetor.java
@@ -1,14 +1,34 @@
 package io.xt.interceptor;
 
+import com.alibaba.fastjson.JSON;
+import io.xt.dto.LoginIofo;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import java.util.Date;
+
 @Component
 public class TokenIntercepetor implements HandlerInterceptor  {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws UnsupportedEncodingException {
+        String authorizationStr = request.getHeader("Authorization");
+        String[] s = authorizationStr.split(" ");
+        String token = s[1];
+        byte[] decode = Base64.getDecoder().decode(token);
+        String loginJsonStr = new String(decode, "UTF-8");
+        LoginIofo loginIofo = JSON.parseObject(loginJsonStr, LoginIofo.class);
+        String username = loginIofo.getUsername();
+        long expireTimestamp = loginIofo.getExpirationTime().getTime();
+        Date currentTime = new Date();
+        Long currentTimestamp = currentTime.getTime();
+        if (username!=null&&!username.isEmpty()&&(currentTimestamp<expireTimestamp)) {
+            return true;
+        }
+
         return false;
     }
 }
