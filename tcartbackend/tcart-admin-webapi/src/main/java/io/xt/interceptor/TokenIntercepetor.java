592e266a93eecbc07da219246d810e95f347b782
Wattingfor-XW <931217673@qq.com>
2019-01-02 23:51:53 +0800

    add token

--- tcartbackend/tcart-admin-webapi/src/main/java/io/xt/interceptor/TokenIntercepetor.java
+++ tcartbackend/tcart-admin-webapi/src/main/java/io/xt/interceptor/TokenIntercepetor.java
@@ -2,6 +2,7 @@ package io.xt.interceptor;
 
 import com.alibaba.fastjson.JSON;
 import io.xt.dto.LoginIofo;
+import io.xt.exception.BackendUnauthenticationException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -14,7 +15,7 @@ import java.util.Date;
 @Component
 public class TokenIntercepetor implements HandlerInterceptor  {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws UnsupportedEncodingException {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws UnsupportedEncodingException, BackendUnauthenticationException {
         String authorizationStr = request.getHeader("Authorization");
         String[] s = authorizationStr.split(" ");
         String token = s[1];
@@ -22,13 +23,16 @@ public class TokenIntercepetor implements HandlerInterceptor  {
         String loginJsonStr = new String(decode, "UTF-8");
         LoginIofo loginIofo = JSON.parseObject(loginJsonStr, LoginIofo.class);
         String username = loginIofo.getUsername();
+        if (username==null){
+            throw new BackendUnauthenticationException("Unauthentication");
+        }
         long expireTimestamp = loginIofo.getExpirationTime().getTime();
         Date currentTime = new Date();
         Long currentTimestamp = currentTime.getTime();
-        if (username!=null&&!username.isEmpty()&&(currentTimestamp<expireTimestamp)) {
-            return true;
+        if (username==null&&username.isEmpty()&&(currentTimestamp>expireTimestamp)) {
+            throw new BackendUnauthenticationException("Unauthentication");
         }
 
-        return false;
+        return true;
     }
 }
