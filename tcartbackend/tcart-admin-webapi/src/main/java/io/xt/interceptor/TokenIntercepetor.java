97fa2db3be275e365807cbdf3c3f4759ad04a779
Wattingfor-XW <931217673@qq.com>
2019-01-02 23:10:44 +0800

    add token

--- tcartbackend/tcart-admin-webapi/src/main/java/io/xt/interceptor/TokenIntercepetor.java
+++ tcartbackend/tcart-admin-webapi/src/main/java/io/xt/interceptor/TokenIntercepetor.java
@@ -1,10 +1,11 @@
 package io.xt.interceptor;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@Component
 public class TokenIntercepetor implements HandlerInterceptor  {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
