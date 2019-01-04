cc3bfda8c2c722d71493df5e54a4b41c5d04f613
Wattingfor-XW <931217673@qq.com>
2019-01-04 19:48:04 +0800

    add batchDelect

---
+++ tcartbackend/tcart-admin-webapi/src/main/java/io/xt/config/text.java
@@ -0,0 +1,15 @@
+package io.xt.config;
+
+public class text {
+    public static void main(String[] args) {
+        for(int i = 0 ; i<5;i++){
+            System.out.println(i);
+            if(i%2==0){
+                System.out.println(i);
+            }else{
+                i++;
+            }
+
+        }
+    }
+}
